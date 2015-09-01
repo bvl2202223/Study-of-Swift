c8987c2f7ef3b2edae97d3ec4a9b0c7fa89041f6
bvl2202223 <binaryvioletlight2202223+bithug@gmail.com>
2015-09-01 19:10:34 +0900

    Initial Commit

---
+++ Study of Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Study of Swift
+//
+//  Created by takahiro baba on 2015/09/01.
+//  Copyright (c) 2015年 bvl. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
